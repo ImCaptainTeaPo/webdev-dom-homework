8565c09037b6e6f7a2c7978947d5ccfbf207033b
ImCaptainTeaPo <SSS1999123@gmail.com>
2025-04-18 15:49:00 +0300

    try1

--- js/api.js
+++ js/api.js
@@ -2,36 +2,60 @@
 const API_URL = 'https://wedev-api.sky.pro/api/v1/golovin-semen/comments'
 
 export async function fetchComments() {
-    const response = await fetch(API_URL)
-    if (!response.ok) {
-        throw new Error('Ошибка загрузки комментариев')
+    try {
+        const response = await fetch(API_URL)
+        if (!response.ok) {
+            if (response.status >= 500) {
+                throw new Error('Сервер сломался, попробуй позже')
+            }
+            throw new Error('Ошибка загрузки комментариев')
+        }
+        const data = await response.json()
+        return data.comments.map((comment) => ({
+            name: comment.author.name,
+            date: new Date(comment.date).toLocaleString(),
+            text: comment.text,
+            likes: comment.likes,
+            isLiked: false,
+        }))
+    } catch (error) {
+        if (error.name === 'TypeError') {
+            throw new Error(
+                'Кажется, у вас сломался интернет, попробуйте позже',
+            )
+        }
+        throw error
     }
-    const data = await response.json()
-    return data.comments.map((comment) => ({
-        name: comment.author.name,
-        date: new Date(comment.date).toLocaleString(),
-        text: comment.text,
-        likes: comment.likes,
-        isLiked: false,
-    }))
 }
 
 export async function postComment({ name, text }) {
-    const response = await fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify({ name, text }),
-    })
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            body: JSON.stringify({ name, text, forceError: true }),
+        })
 
-    if (response.status === 400) {
-        const errorData = await response.json()
-        throw new Error(errorData.error)
+        if (response.status === 400) {
+            const errorData = await response.json()
+            throw new Error(errorData.error)
+        }
+
+        if (!response.ok) {
+            if (response.status >= 500) {
+                throw new Error('Сервер сломался, попробуй позже')
+            }
+            throw new Error('Ошибка отправки комментария')
+        }
+
+        return response.json()
+    } catch (error) {
+        if (error.name === 'TypeError') {
+            throw new Error(
+                'Кажется, у вас сломался интернет, попробуйте позже',
+            )
+        }
+        throw error
     }
-
-    if (!response.ok) {
-        throw new Error('Ошибка отправки комментария')
-    }
-
-    return response.json()
 }
 
 export function getCurrentDateTime() {
